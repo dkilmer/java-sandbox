c2f34d2b6ea2d37b8a3b55649c74a6823fc8f7e1
Devonna Leslie <25532118+dleslie28@users.noreply.github.com>
2020-07-26 12:56:20 -0400

    n/a

--- src/main/java/org/ssanames/project/Main.java
+++ src/main/java/org/ssanames/project/Main.java
@@ -7,14 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-        ParseBabyNames pb = new ParseBabyNames("SsaNames.db");
-        NamesAnalysis na = new NamesAnalysis("SsaNames.db");
-        GenerateHtml html = new GenerateHtml();
-        //pb.createTable(2010);
-        //pb.loadTable(2010);
-        //na.getRankChange("Madison", 'F', 2017, 2018);
-        na.LineChart("Devonna", 'F');
-        html.GenerateHtml("Devonna");
+            ParseBabyNames pb = new ParseBabyNames("SsaNames.db");
+            NamesAnalysis na = new NamesAnalysis("SsaNames.db");
+            GenerateHtml html = new GenerateHtml();
+            //pb.createTable(2010);
+            //pb.loadTable(2010);
+            //na.getRankChange("Madison", 'F', 2017, 2018);
+            na.LineChart("Devonna", 'F');
+            html.GenerateHtml("Devonna");
         } catch (IOException ie) {
             System.err.println("I/O error: " + ie.getMessage());
         }
